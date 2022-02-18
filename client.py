b24c39d083d3517aaa9a1c7d325912e5898aece4
Julio Salazar <juliocsp03@gmail.com>
2022-02-18 16:34:21 -0600

    Cliente/Server envía y recibe datos

--- client.py
+++ client.py
@@ -1,31 +1,43 @@
 # IMPORTAMOS LAS LIBRERÍAS NECESARIAS
 import socket
-
+import os
 # CREAMOS UN NUEVO SOCKET
 s = socket.socket()
 # DEFINIMOS EL PUERTO
 port = 8000
-path = ""
+# path = "dirx/"
 # CREAMOS LA VARIABLE DE LA CABECERA
 header = """\
-PUT / HTTP/1.1\r
+{verbo_http} {ruta} / HTTP/1.1\r
 Content-Type: {content_type}\r
 Content-Length: {content_length}\r
 Host: {host}\r
 Connection: close\r
 \r\n"""
+
+# os.makedirs(os.path.dirname(path), exist_ok=True)
+file = open('grupo_2/g2_archivo_75', 'rt')
+ruta_server = "dirx/filex"
+
 # VARIABLE DEL BODY DE LA PETICIÓN
-body = "Test to server".encode()
+v1 = file.read()
+body = v1.encode()
 header_encode = header.format(
+	verbo_http="PUT",
+	ruta=ruta_server,
     content_type="application/x-www-form-urlencoded",
     content_length=len(body),
     host=str('localhost') + ":" + str(port)
 ).encode()
 
-payload = header_encode + body
+# payload = header_encode + body
+payload = body
 # CREAMOS UNA CONEXIÓN CON LOCALHOST EN EL PUERTO DEFINIDO
 s.connect(('localhost', port))
+# ENVIAMOS EL PAYLOAD AL SERVIDOR
 s.sendall(payload)
+# IMPRIMIMOS
 print(payload.decode())
 print(s.recv(1024).decode())
+# CERRAMOS LA CONEXIÓN
 s.close()
--- server1.py
+++ server1.py
@@ -8,10 +8,14 @@ s.bind(('', port))
 
 s.listen(5)
 
+c, addr = s.accept()
 while True:
-	c, addr = s.accept()
-	# c.send("Recibido".encode())
+	c.send("Recibido".encode())
+	# data = c.recv(1024).decode()
+	print(c)
+	print(addr)
 	print(c.recv(1024).decode())
 	c.close()
 	break
 	pass
+
