271969c108fab39ecfbeaacc54fab8ee898d38e5
RicardoIbarra9 <1730053@upv.edu.mx>
2022-04-04 15:32:56 -0500

    Balanceadores y descargas

---
+++ README.txt
@@ -0,0 +1,8 @@
+Ejecutar primero generator.py, después ejecutar server1.py y finalmente client.py
+
+
+python3 server1.py localhost 46001
+python3 server2.py localhost 46002
+python3 server3.py localhost 46003
+
+python3 test.py
---
+++ generator.py
@@ -0,0 +1,24 @@
+# GENERA ARCHIVOS DE DIFERENTES TAMAÑOS
+import os
+import random
+grupo = 1
+print("Generando archivos ...")
+for g in range(3):
+	path = 'grupo_' + str(grupo) + '/'
+	os.makedirs(os.path.dirname(path), exist_ok=True)
+	print("grupo_" + str(g+1))
+	for x in range(100):
+		with open(path + 'g' + str(grupo) + '_archivo_' + str(x+1), 'wt') as f:
+			if grupo == 1:
+				num_chars = random.randint(1, 1024*10)
+			elif grupo == 2:
+				num_chars = random.randint(1024*10, 1024*1000)
+			elif grupo == 3:
+				num_chars = random.randint(1024*1000, 1024*10000)
+			else:
+				break
+			f.write('0' * num_chars)
+		if x + 1  == 100:
+			grupo = grupo + 1
+		pass
+	pass
